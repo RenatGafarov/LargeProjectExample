70382ca0d920f0d0da09a4399b4edea2c4f7dd97
Вуцинс Айварс Гатисович <avucins@dev.vtb.ru>
2021-12-07 01:01:16 +0300

    Initial Commit

---
+++ test13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test13
+//
+//  Created by kushvid on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
